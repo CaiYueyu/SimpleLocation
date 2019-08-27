8cafd9b0a61577aa9d9e965d84eaf543ec04a042
ZMJ <202127816@qq.com>
2019-08-27 23:41:19 +0800

    Initial commit

---
+++ simplelocationdemo/src/main/java/com/mjzuo/simplelocation/LocationTaskActivity.java
@@ -0,0 +1,14 @@
+package com.mjzuo.simplelocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_task_activity);
+    }
+}
